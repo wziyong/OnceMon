651a01d6ad5e7b1af94ceef35924b918e62b4d5e
gzy <guzychina@163.com>
2015-01-04 17:18:20 +0800

    OnceMon Configuration Completed

---
+++ conf/zabbix.conf.php
@@ -0,0 +1,20 @@
+<?php
+// Zabbix GUI configuration file
+global $DB;
+
+$DB['TYPE']     = 'MYSQL';
+$DB['SERVER']   = '133.133.135.7';
+$DB['PORT']     = '3306';
+$DB['DATABASE'] = 'zabbix';
+$DB['USER']     = 'root';
+$DB['PASSWORD'] = 'onceas';
+
+// SCHEMA is relevant only for IBM_DB2 database
+$DB['SCHEMA'] = '';
+
+$ZBX_SERVER      = '133.133.135.7';
+$ZBX_SERVER_PORT = '10051';
+$ZBX_SERVER_NAME = 'zabbix-server';
+
+$IMAGE_FORMAT_DEFAULT = IMAGE_FORMAT_PNG;
+?>
