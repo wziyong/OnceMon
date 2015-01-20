7528e020f374e1f954ac67b1694361966c43e7f5
hehai0803 <hehai@vip.126.com>
2015-01-20 18:56:36 +0800

    界面改版成Metro风格，炫酷

--- include/page_header.php
+++ include/page_header.php
@@ -200,60 +200,22 @@ if (defined('ZBX_PAGE_NO_HEADER')) {
 }
 
 if (!defined('ZBX_PAGE_NO_MENU')) {
-	$help = new CLink(_('Help'), 'http://www.iscas.ac.cn/', 'small_font', null, 'nosid');
-	$help->setTarget('_blank');
-	$support = new CLink(_('Get support'), 'http://www.iscas.ac.cn/', 'small_font', null, 'nosid');
-	$support->setTarget('_blank');
 
-	$printview = new CLink(_('Print'), '', 'small_font print-link', null, 'nosid');
+//	$logo = new CLink(new CDiv(SPACE, 'zabbix_logo'), 'http://www.iscas.ac.cn/', 'image', null, 'nosid');
+//	$logo->setTarget('_blank');
 
-	$page_header_r_col = array($help, '|', $support, '|', $printview, '|');
+//	$top_page_row = array(
+//		new CCol(null, 'page_header_l'),
+//		new CCol($page_header_r_col, 'maxwidth page_header_r')
+//	);
 
-	if (!CWebUser::isGuest()) {
-		array_push($page_header_r_col, new CLink(_('Profile'), 'profile.php', 'small_font', null, 'nosid'), '|');
-	}
+//	unset($logo, $page_header_r_col, $help, $support);
 
-	if (isset(CWebUser::$data['debug_mode']) && CWebUser::$data['debug_mode'] == GROUP_DEBUG_MODE_ENABLED) {
-		$debug = new CLink(_('Debug'), '#debug', 'small_font', null, 'nosid');
-		$d_script = " if (!isset('state', this)) { this.state = 'none'; }".
-			" if (this.state == 'none') { this.state = 'block'; }".
-			" else { this.state = 'none'; }".
-			" showHideByName('zbx_debug_info', this.state);";
-		$debug->setAttribute('onclick', 'javascript: '.$d_script);
-		array_push($page_header_r_col, $debug, '|');
-	}
-
-	if (CWebUser::isGuest()) {
-		$page_header_r_col[] = array(new CLink(_('Login'), 'index.php?reconnect=1', 'small_font', null, null));
-	}
-	else {
-		// it is not possible to logout from HTTP authentication
-		$chck = $page['file'] == 'authentication.php' && isset($_REQUEST['save'], $_REQUEST['config']);
-		if ($chck && $_REQUEST['config'] == ZBX_AUTH_HTTP || !$chck && isset($config) && $config['authentication_type'] == ZBX_AUTH_HTTP) {
-			$logout =  new CLink(_('Logout'), '', 'small_font', null, 'nosid');
-			$logout->setHint(_s('It is not possible to logout from HTTP authentication.'), null, null, false);
-		}
-		else {
-			$logout =  new CLink(_('Logout'), 'index.php?reconnect=1', 'small_font', null, null);
-		}
-		array_push($page_header_r_col, $logout);
-	}
-
-	$logo = new CLink(new CDiv(SPACE, 'zabbix_logo'), 'http://www.iscas.ac.cn/', 'image', null, 'nosid');
-	$logo->setTarget('_blank');
-
-	$top_page_row = array(
-		new CCol($logo, 'page_header_l'),
-		new CCol($page_header_r_col, 'maxwidth page_header_r')
-	);
-
-	unset($logo, $page_header_r_col, $help, $support);
-
-	$table = new CTable(null, 'maxwidth page_header');
-	$table->setCellSpacing(0);
-	$table->setCellPadding(5);
-	$table->addRow($top_page_row);
-	$table->show();
+//	$table = new CTable(null, 'maxwidth page_header');
+//	$table->setCellSpacing(0);
+//	$table->setCellPadding(5);
+//	$table->addRow($top_page_row);
+//	$table->show();
 
 	$menu_table = new CTable(null, 'menu pointer');
 	$menu_table->setCellSpacing(0);
@@ -338,26 +300,74 @@ if (!defined('ZBX_PAGE_NO_MENU')) {
 		}
 	}
 
-	if (isset($ZBX_SERVER_NAME) && !zbx_empty($ZBX_SERVER_NAME)) {
-		$table = new CTable();
-		$table->addStyle('width: 100%;');
+//	if (isset($ZBX_SERVER_NAME) && !zbx_empty($ZBX_SERVER_NAME)) {
+//		$table = new CTable();
+//		$table->addStyle('width: 100%;');
+//
+//		$tableColumn = new CCol(new CSpan($ZBX_SERVER_NAME, 'textcolorstyles'));
+//		if (is_null($node_form)) {
+//			$tableColumn->addStyle('padding-right: 5px;');
+//		}
+//		else {
+//			$tableColumn->addStyle('padding-right: 20px; padding-bottom: 2px;');
+//		}
+//		$table->addRow(array($tableColumn, $node_form));
+//		$node_form = $table;
+//	}
 
-		$tableColumn = new CCol(new CSpan($ZBX_SERVER_NAME, 'textcolorstyles'));
-		if (is_null($node_form)) {
-			$tableColumn->addStyle('padding-right: 5px;');
-		}
-		else {
-			$tableColumn->addStyle('padding-right: 20px; padding-bottom: 2px;');
-		}
-		$table->addRow(array($tableColumn, $node_form));
-		$node_form = $table;
+	if (isset($ZBX_SERVER_NAME) && !zbx_empty($ZBX_SERVER_NAME)) {
+		$oncemon_name = new CLink($ZBX_SERVER_NAME, '#', 'small_font', null, null);
 	}
 
+	$help = new CLink(_('Help'), 'http://www.iscas.ac.cn/', 'small_font', null, 'nosid');
+	$help->setTarget('_blank');
+
+	$printview = new CLink(_('Print'), '', 'small_font print-link', null, 'nosid');
+
+	$page_header_r_col = array($oncemon_name, '|', $help, '|', $printview, '|');
+
+	if (!CWebUser::isGuest()) {
+		array_push($page_header_r_col, new CLink(_('Profile'), 'profile.php', 'small_font', null, 'nosid'), '|');
+	}
+
+	if (isset(CWebUser::$data['debug_mode']) && CWebUser::$data['debug_mode'] == GROUP_DEBUG_MODE_ENABLED) {
+		$debug = new CLink(_('Debug'), '#debug', 'small_font', null, 'nosid');
+		$d_script = " if (!isset('state', this)) { this.state = 'none'; }".
+			" if (this.state == 'none') { this.state = 'block'; }".
+			" else { this.state = 'none'; }".
+			" showHideByName('zbx_debug_info', this.state);";
+		$debug->setAttribute('onclick', 'javascript: '.$d_script);
+		array_push($page_header_r_col, $debug, '|');
+	}
+
+	if (CWebUser::isGuest()) {
+		$page_header_r_col[] = array(new CLink(_('Login'), 'index.php?reconnect=1', 'small_font', null, null));
+	}
+	else {
+		// it is not possible to logout from HTTP authentication
+		$chck = $page['file'] == 'authentication.php' && isset($_REQUEST['save'], $_REQUEST['config']);
+		if ($chck && $_REQUEST['config'] == ZBX_AUTH_HTTP || !$chck && isset($config) && $config['authentication_type'] == ZBX_AUTH_HTTP) {
+			$logout =  new CLink(_('Logout'), '', 'small_font', null, 'nosid');
+			$logout->setHint(_s('It is not possible to logout from HTTP authentication.'), null, null, false);
+		}
+		else {
+			$logout =  new CLink(_('Logout'), 'index.php?reconnect=1', 'small_font', null, null);
+		}
+		$logout->setAttribute('style', 'margin-right: 5px');
+		array_push($page_header_r_col, $logout);
+	}
+
+	unset($help);
 	// 1st level menu
 	$table = new CTable(null, 'maxwidth');
-	$r_col = new CCol($node_form, 'right');
+//	$r_col = new CCol($node_form, 'right');
+//	$r_col->setAttribute('style', 'line-height: 1.8em;');
+//	$table->addRow(array($menu_table, $r_col));
+	$r_col = new CCol($page_header_r_col, 'right');
 	$r_col->setAttribute('style', 'line-height: 1.8em;');
 	$table->addRow(array($menu_table, $r_col));
+
+	unset($page_header_r_col);
 
 	$page_menu = new CDiv(null, 'textwhite');
 	$page_menu->setAttribute('id', 'mmenu');
