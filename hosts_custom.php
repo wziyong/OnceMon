931e298399431d14796af924e7a04ce25decb841
wziyong <wziyong@126.com>
2015-09-18 16:15:43 +0800

    修改查询主机列表；

--- hosts_custom.php
+++ hosts_custom.php
@@ -2,7 +2,8 @@
 require_once dirname(__FILE__).'/include/config.inc.php';
 require_once dirname(__FILE__).'/include/forms.inc.php';
 
-$parentHosts = API::Host()->getParentHosts(32);
+$grouid = $_REQUEST['groupid'];
+$parentHosts = API::Host()->getParentHosts($grouid);
 echo json_encode($parentHosts);
 
 
--- include/views/js/custom/custom.host.edit.js.php
+++ include/views/js/custom/custom.host.edit.js.php
@@ -1,10 +1,15 @@
 <script type = "text/javascript" >
     jQuery(document).ready(function () {
         setServerCfg();
-        getPartentHosts();
+        getPartentHosts(jQuery("#groups").val());
 
         jQuery('#server_type').on('change', function () {
             setServerCfg();
+        });
+
+        jQuery('#groups').on('change', function () {
+            var groupid = jQuery("#groups").val();
+            getPartentHosts(groupid);
         });
 
         function setServerCfg() {
@@ -14,51 +19,65 @@
                 jQuery('#label_server_cfg').removeClass('hidden');
                 jQuery('.app_server').addClass('hidden');
                 jQuery('.lbs_server').removeClass('hidden');
+                jQuery("#parentid").prepend("<option value='0'>根节点</option>")
             } else if ('1' == selected) {
                 jQuery('#label_server_cfg').removeClass('hidden');
                 jQuery('.lbs_server').addClass('hidden');
                 jQuery('#app_http_port').attr('style','width: 312px;');
                 jQuery('.app_server').removeClass('hidden');
+                jQuery("#parentid option[value='0']").remove();
             }
             else {
                 jQuery('#label_server_cfg').addClass('hidden');
                 jQuery('.app_server').addClass('hidden');
                 jQuery('.lbs_server').addClass('hidden');
+                jQuery("#parentid option[value='0']").remove();
             }
         }
 
-        function getPartentHosts()
+        function getPartentHosts(groupid)
         {
+            var server_type = jQuery('#server_type').val();
             var parentIdHidden = jQuery('#parentIdHidden').val();
             var hostIdHidden = jQuery('#hostIdHidden').val();
             var ajaxUrl = new Curl('hosts_custom.php');
             var ajaxRequest = jQuery.ajax({
                 url: ajaxUrl.getUrl(),
                 type: 'post',
-                data: {},
+                data: {groupid:groupid},
                 success: function (data) {
                     jQuery('#parentid').empty();
-                    if(parentIdHidden==0)
-                    {
-                        jQuery('#parentid').append('<option selected="selected" value="0">根节点</option>');
-                    }
-
                     var json = eval('(' + data + ')');
-                    for (var name in json) {//遍历json对象的每个key/value对,p为key
-                        if(hostIdHidden != name)
+                    if(!jQuery.isEmptyObject(json) && data && data!='[]')
+                    {
+                        if(parentIdHidden==0)
                         {
-                            if(parentIdHidden == name)
+                            jQuery('#parentid').append('<option selected="selected" value="0">根节点</option>');
+                        }
+
+                        for (var name in json) {//遍历json对象的每个key/value对,p为key
+                            if(hostIdHidden != name)
                             {
-                                jQuery('#parentid').append('<option selected="selected" value="' + name + '">' + json[name] + '</option>');
-                            }else
-                            {
-                                jQuery('#parentid').append('<option value="' + name + '">' + json[name] + '</option>');
+                                if(parentIdHidden == name)
+                                {
+                                    jQuery('#parentid').append('<option selected="selected" value="' + name + '">' + json[name] + '</option>');
+                                }else
+                                {
+                                    jQuery('#parentid').append('<option value="' + name + '">' + json[name] + '</option>');
+                                }
                             }
+                        }
+                    }
+                    else
+                    {
+                        if(server_type == '0')
+                        {
+                            jQuery('#parentid').append('<option selected="selected" value="0">根节点</option>');
                         }
                     }
                 },
                 error: function(xx) {
-                   // alert('error'+xx);
+                    alert('查询父节点失败！');
                 }
             });
         }
