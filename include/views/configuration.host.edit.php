a037d5a894d3e030623eb9521fb11e795b6d7710
wziyong <wziyong@126.com>
2015-09-21 11:12:57 +0800

    新增主机，删除最下的操作按钮，clone；

--- include/views/configuration.host.edit.php
+++ include/views/configuration.host.edit.php
@@ -1006,8 +1006,8 @@ $frmHost->addItem($divTabs);
 $others = array();
 // do not display the clone and delete buttons for clone forms and new host forms
 if (getRequest('hostid') && !in_array(getRequest('form'), array('clone', 'full_clone'))) {
-	$others[] = new CSubmit('clone', _('Clone'));
-	$others[] = new CSubmit('full_clone', _('Full clone'));
+	//$others[] = new CSubmit('clone', _('Clone'));
+	//$others[] = new CSubmit('full_clone', _('Full clone'));
 	$others[] = new CButtonDelete(_('Delete selected host?'), url_param('form').url_param('hostid').url_param('groupid'));
 }
 $others[] = new CButtonCancel(url_param('groupid'));
